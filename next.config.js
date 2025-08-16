ca3e5b098ecbb6e7163e8ab30215cb21cdc8cbd1
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-16 23:42:12 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: { ignoreBuildErrors: true },  // belt-and-suspenders
+  output: 'export',                // generate ./out for static hosting
+  typescript: { ignoreBuildErrors: false },
   eslint: { ignoreDuringBuilds: true }
 };
 module.exports = nextConfig;
