5ea2d9b4436e13060118ef8c023a91d7172323f3
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-17 03:06:47 +1000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <section>
-      <h1>WatchValu8r</h1>
+      <h1>WatchGauge</h1>
       <p>If you can read this, the Amplify build + static export worked 🎉</p>
     </section>
   );
