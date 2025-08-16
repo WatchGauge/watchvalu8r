650633400cffa2e8d10a7a4c042090deba23ba61
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-16 22:11:20 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,4 @@
 export default function HomePage() {
-  return (
-    <section>
-      <h1>WatchValu8r</h1>
-      <p>If you can read this, the Amplify build and static export worked 🎉</p>
-    </section>
-  );
+  return (<section><h1>WatchValu8r</h1><p>If you can read this, the Amplify build + static export worked 🎉</p></section>);
 }
 
