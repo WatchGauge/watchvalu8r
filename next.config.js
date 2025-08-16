38d76ee1f54c8bc054b51bf6b308ec5bddf4aa68
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-16 22:18:42 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: 'export', typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } };
+const nextConfig = {
+  output: 'export',              // generate static HTML to ./out
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
+};
 module.exports = nextConfig;
