3cbf1d0e64c7ee33af55b53d22052a4d84544c58
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-18 14:25:48 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'   // <- required for static files in /out
+  output: 'export'
 };
 module.exports = nextConfig;
-
-
