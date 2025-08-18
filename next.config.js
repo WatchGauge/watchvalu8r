8e1e8addb623a54796f452995b82c7f9eb40655a
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-18 13:45:21 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Do NOT set `output: 'export'` for Amplify SSR hosting.
-  // Leave below options as-is unless you know you need them.
-
-  // images: { domains: ['example.com'] }, // <- add remote image domains here
-  // trailingSlash: false,
-  // poweredByHeader: false,
+  // Enable static export so Amplify can serve /index.html from the 'out' folder
+  output: 'export',
 };
-
 module.exports = nextConfig;
