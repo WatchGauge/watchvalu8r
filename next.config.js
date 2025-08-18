19826649b93abdfead1fd41d394d3fffb715f580
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-18 14:08:03 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Static export so Amplify hosts /out/index.html and avoids 404s
   output: 'export'
 };
 module.exports = nextConfig;
+
 
