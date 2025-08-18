655aec20e97165c53a04d6b5e0baa87c52ab0a0f
WatchGauge <davidrhysjenkins100@gmail.com>
2025-08-18 13:41:17 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // This makes Next output a static site into ./out
-  output: "export"
+  reactStrictMode: true,
+  // Do NOT set `output: 'export'` for Amplify SSR hosting.
+  // Leave below options as-is unless you know you need them.
+
+  // images: { domains: ['example.com'] }, // <- add remote image domains here
+  // trailingSlash: false,
+  // poweredByHeader: false,
 };
+
 module.exports = nextConfig;
